dcb12b530fa10bc1b1ceb352e55f26888cea96df
Sidarth G <58748190+sidarth-23@users.noreply.github.com>
2024-01-04 23:03:53 +0530

    Add heading to Weather Alerts page

--- src/app/alerts/page.tsx
+++ src/app/alerts/page.tsx
@@ -4,6 +4,7 @@ import InputBox from "./_components/input-box";
 export default function WeatherAlertsPage() {
     return (
         <div className="flex flex-col justify-center items-center gap-10 pt-4">
+            <h1 className="text-2xl ">Weather Alerts</h1>
             <InputBox />
             <DataBox />
         </div>
